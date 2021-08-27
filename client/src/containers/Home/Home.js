68be7cebf742e8802e6dfbd838885be6b4c25a92
manyimy <wong.manyi19@gmail.com>
2021-08-27 15:19:29 +0800

    fix: make Home class

--- client/src/containers/Home/Home.js
+++ client/src/containers/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
@@ -16,7 +16,7 @@ import PSTable from '../../components/Home/psTable';
 import ReactHTMLTableToExcel from 'react-html-table-to-excel';
 import { getTheme } from '../Setting/settingsReducer';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = withStyles((theme) => ({
   formControl: {
     margin: theme.spacing(1),
     minWidth: "100%",
@@ -48,166 +48,184 @@ const useStyles = makeStyles((theme) => ({
   // },
 }));
 
-export default function Home() {
-  const classes = useStyles();
-  const steps = getSteps();
-  // const [checked, setChecked] = React.useState([]);
-  const [activeStep, setActiveStep] = React.useState(1);
-  const [state, setState] = React.useState({
-    intake: '',
-    spec: '',
-  });
 
-  // step navigation
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+const steps = getSteps();
+// const [checked, setChecked] = React.useState([]);
+// const [activeStep, setActiveStep] = React.useState(0);
+// const [state, setState] = React.useState({
+//   intake: '',
+//   spec: '',
+// });
+
+// step navigation
+
+
+export default useStyles(class Home extends React.Component {
+  state = {
+    activeStep: 0,
+    data: {
+      intake: '',
+      spec: ''
+    }
+  }
+
+  handleNext = () => {
+    let prevActiveStep = this.state.activeStep;
+    this.setState({ activeStep: prevActiveStep + 1 })
+    // setActiveStep((prevActiveStep) => prevActiveStep + 1);
   };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  
+  handleBack = () => {
+    let prevActiveStep = this.state.activeStep;
+    this.setState({ activeStep: prevActiveStep - 1 })
+    // setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
-
-  const handleReset = () => {
-    setActiveStep(0);
+  
+  handleReset = () => {
+    this.setState({ activeStep: 0 })
+    // setActiveStep(0);
   };
-
-  const handleChange = (event) => {
+  
+  handleChange = (event) => {
     const name = event.target.name;
-    setState({
-      ...state,
+    this.setState({
+      ...this.state.data,
       [name]: event.target.value,
     });
   };
-
-  return (
-    <div className={classes.container}>
-      <Stepper alternativeLabel activeStep={activeStep} connector={<ColorlibConnector />}>
-        {steps.map((label) => (
-          <Step key={label}>
-            <StepLabel StepIconComponent={ColorlibStepIcon}>{label}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
-      <div>
-        {activeStep === steps.length ? (
-          <div>
-            <Button onClick={handleReset} className={classes.button}>
-              Reset
-            </Button>
-          </div>
-        ) :
-        activeStep === 0 ? (
-          <div>
-            <Grid container spacing={3}>
-              <Grid item xs>
-                <Paper className={classes.paper}></Paper>
-              </Grid>
-              <Grid item xs={2}>
-                <FormControl required className={classes.formControl}>
-                  <InputLabel htmlFor="age-native-simple">Intake</InputLabel>
-                  <Select
-                    native
-                    value={state.intake}
-                    onChange={handleChange}
-                    inputProps={{
-                      name: 'intake',
-                      id: 'intake-native-simple',
-                    }}
-                  >
-                    <option aria-label="None" value="" />
-                    <option value={"April 2021"}>April 2021</option>
-                    <option value={"July 2021"}>July 2021</option>
-                    <option value={"September 2021"}>September 2021</option>
-                  </Select>
-                  <FormHelperText>Required</FormHelperText>
-                </FormControl>
-              </Grid>
-              <Grid item xs={3}>
-                <FormControl required className={classes.formControl}>
-                  <InputLabel htmlFor="age-native-simple">Specialization</InputLabel>
-                  <Select
-                    native
-                    value={state.spec}
-                    onChange={handleChange}
-                    inputProps={{
-                      name: 'spec',
-                      id: 'spec-native-simple',
-                    }}
-                  >
-                    <option aria-label="None" value="" />
-                    <option value={"Software Engineering"}>Software Engineering</option>
-                    <option value={"Game Development"}>Game Development</option>
-                    <option value={"Data Science"}>Data Science</option>
-                    <option value={"Cybersecurity"}>Cybersecurity</option>
-                  </Select>
-                  <FormHelperText>Required</FormHelperText>
-                </FormControl>
-              </Grid>
-              <Grid item xs>
-                <Paper className={classes.paper}></Paper>
-              </Grid>
-            </Grid>
-            <Grid container spacing={3} >
-              <Grid item xs>
-                <Paper className={classes.paper}></Paper>
-              </Grid>
-              <Grid item xs={5}>
-                <TransferList />
-              </Grid>
-              <Grid item xs>
-                <Paper className={classes.paper}></Paper>
-              </Grid>
-            </Grid>
-            <div className={classes.btnContainer}>
-              <Button disabled={activeStep === 0} onClick={handleBack} className={classes.button}>
-                Back
-              </Button>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={handleNext}
-                className={classes.button}
-              >
-                Next
+  
+  render(){
+    const { classes } = this.props;
+    return (
+      <div className={classes.container}>
+        <Stepper alternativeLabel activeStep={this.state.activeStep} connector={<ColorlibConnector />}>
+          {steps.map((label) => (
+            <Step key={label}>
+              <StepLabel StepIconComponent={ColorlibStepIcon}>{label}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+        <div>
+          {this.state.activeStep === steps.length ? (
+            <div>
+              <Button onClick={this.handleReset} className={classes.button}>
+                Reset
               </Button>
             </div>
-          </div>
-        ) : (
-          <div className={classes.container}>
-            <Grid container spacing={3} >
-              <Grid item xs>
-                <Paper className={classes.paper}></Paper>
+          ) :
+          this.state.activeStep === 0 ? (
+            <div>
+              <Grid container spacing={3}>
+                <Grid item xs>
+                  <Paper className={classes.paper}></Paper>
+                </Grid>
+                <Grid item xs={2}>
+                  <FormControl required className={classes.formControl}>
+                    <InputLabel htmlFor="age-native-simple">Intake</InputLabel>
+                    <Select
+                      native
+                      value={this.state.data.intake}
+                      onChange={this.handleChange}
+                      inputProps={{
+                        name: 'intake',
+                        id: 'intake-native-simple',
+                      }}
+                    >
+                      <option aria-label="None" value="" />
+                      <option value={"April 2021"}>April 2021</option>
+                      <option value={"July 2021"}>July 2021</option>
+                      <option value={"September 2021"}>September 2021</option>
+                    </Select>
+                    <FormHelperText>Required</FormHelperText>
+                  </FormControl>
+                </Grid>
+                <Grid item xs={3}>
+                  <FormControl required className={classes.formControl}>
+                    <InputLabel htmlFor="age-native-simple">Specialization</InputLabel>
+                    <Select
+                      native
+                      value={this.state.data.spec}
+                      onChange={this.handleChange}
+                      inputProps={{
+                        name: 'spec',
+                        id: 'spec-native-simple',
+                      }}
+                    >
+                      <option aria-label="None" value="" />
+                      <option value={"Software Engineering"}>Software Engineering</option>
+                      <option value={"Game Development"}>Game Development</option>
+                      <option value={"Data Science"}>Data Science</option>
+                      <option value={"Cybersecurity"}>Cybersecurity</option>
+                    </Select>
+                    <FormHelperText>Required</FormHelperText>
+                  </FormControl>
+                </Grid>
+                <Grid item xs>
+                  <Paper className={classes.paper}></Paper>
+                </Grid>
               </Grid>
-              <Grid item xs={12}>
-                <PSTable greeting="Welcome to Rex"/>
+              <Grid container spacing={3} >
+                <Grid item xs>
+                  <Paper className={classes.paper}></Paper>
+                </Grid>
+                <Grid item xs={5}>
+                  <TransferList />
+                </Grid>
+                <Grid item xs>
+                  <Paper className={classes.paper}></Paper>
+                </Grid>
               </Grid>
-              <Grid item xs>
-                <Paper className={classes.paper}></Paper>
-              </Grid>
-            </Grid>
-            <div className={classes.btnContainer}>
-              <Button disabled={activeStep === 0} onClick={handleBack} className={classes.button}>
-                Back
-              </Button>
-              {/* <Button
-                variant="contained"
-                color="primary"
-                onClick={handleNext}
-                className={classes.button}
-              > */}
-                <ReactHTMLTableToExcel
-                  id="test-table-xls-button"
-                  className={classes.downloadBtn + " download-table-xls-button" }
-                  table="table-to-xls"
-                  filename="tablexls"
-                  sheet="tablexls"
-                  buttonText="Download"/>
-                {/* Download */}
-              {/* </Button> */}
+              <div className={classes.btnContainer}>
+                <Button disabled={this.state.activeStep === 0} onClick={this.handleBack} className={classes.button}>
+                  Back
+                </Button>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={this.handleNext}
+                  className={classes.button}
+                >
+                  Next
+                </Button>
+              </div>
             </div>
-          </div>
-        )}
+          ) : (
+            <div className={classes.container}>
+              <Grid container spacing={3} >
+                <Grid item xs>
+                  <Paper className={classes.paper}></Paper>
+                </Grid>
+                <Grid item xs={12}>
+                  <PSTable greeting="Welcome to Rex"/>
+                </Grid>
+                <Grid item xs>
+                  <Paper className={classes.paper}></Paper>
+                </Grid>
+              </Grid>
+              <div className={classes.btnContainer}>
+                <Button disabled={this.state.activeStep === 0} onClick={this.handleBack} className={classes.button}>
+                  Back
+                </Button>
+                {/* <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handleNext}
+                  className={classes.button}
+                > */}
+                  <ReactHTMLTableToExcel
+                    id="test-table-xls-button"
+                    className={classes.downloadBtn + " download-table-xls-button" }
+                    table="table-to-xls"
+                    filename="tablexls"
+                    sheet="tablexls"
+                    buttonText="Download"/>
+                  {/* Download */}
+                {/* </Button> */}
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+});
