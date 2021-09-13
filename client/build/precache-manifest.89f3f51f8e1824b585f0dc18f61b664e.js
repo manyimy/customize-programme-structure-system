8a3122f79818b080517e9662c63a47bc2fbd2b96
manyimy <wong.manyi19@gmail.com>
2021-09-13 21:53:52 +0800

    feat: build folder

---
+++ client/build/precache-manifest.89f3f51f8e1824b585f0dc18f61b664e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8549e5ed1c62702f2bbb849f86fa83cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a38b22192ca6b9e7cdc2",
+    "url": "/static/css/main.6c51ec01.chunk.css"
+  },
+  {
+    "revision": "ddc5b54df40eb14260d1",
+    "url": "/static/js/2.c080ca57.chunk.js"
+  },
+  {
+    "revision": "6ef57ea73f859e79d7f696b27898a6e5",
+    "url": "/static/js/2.c080ca57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a38b22192ca6b9e7cdc2",
+    "url": "/static/js/main.a4e95ebf.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
